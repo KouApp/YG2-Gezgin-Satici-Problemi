aa9a08d5478f40d32f7883c004afc16280d9fb27
Abdullah Ali GÜN <aagun16@gmail.com>
2022-04-19 02:27:24 +0300

    NeighbourNode Add

--- Assets/NodeBehaviour.cs
+++ Assets/NodeBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,58 @@ using UnityEngine.EventSystems;
 
 public class NodeBehaviour : MonoBehaviour
 {
-    Vector2 difference = Vector2.zero;
+    private Vector2 difference = Vector2.zero;
+    private  List<NeighbourNode> neightbours = new();
+
+    private Camera mainCam;
+
+    private void Awake()
+    {
+        mainCam = Camera.main;
+    }
 
     private void OnMouseDown()
     {
-        difference = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position;
+        difference = (Vector2)mainCam.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position;
     }
 
     private void OnMouseDrag()
     {
-        transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - difference;
+        transform.position = (Vector2)mainCam.ScreenToWorldPoint(Input.mousePosition) - difference;
+    }
+    
+    
+    
+    public void AddNeightbour(NeighbourNode node)
+    {
+        node.CalculateDistance(gameObject);
+        neightbours.Add(node);
+    }
+    public void RemoveNeightbour(NodeBehaviour node)
+    {
+        neightbours.Remove(neightbours.Find(x => x.node == node));
+    }
+}
+
+public class NeighbourNode
+{
+    public NodeBehaviour node { get; private set; }
+    public LineRendererCS line { get; private set; }
+    public float distance { get; private set; }
+
+    public NeighbourNode(NodeBehaviour node, LineRendererCS line, float distance)
+    {
+        this.node = node;
+        this.line = line;
+        this.distance = distance;
+    }
+    public void CalculateDistance(GameObject node)
+    {
+        distance = Vector2.Distance(node.transform.position, this.node.transform.position);
+    }
+
+    public void SetDistance(float distance)
+    {
+        this.distance = distance;
     }
 }
